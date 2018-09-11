89df7860c2f581f6ad9de6f40136da75a64c5ba3
root <root@shane-optiplex-980.student.iastate.edu>
2018-09-11 00:39:54 -0500

    Added SocketFactory

---
+++ server/src/socket/Socket.h
@@ -0,0 +1,6 @@
+#ifndef Socket
+# define Socket
+
+Socket* newSocket(unsigned int port);
+
+#endif
---
+++ server/src/socket/SocketFactory.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int sockfd, newsockfd, portno, clilen;
+char buffer[256];
+struct sockaddr_in serv_addr, cli_addr;
+
+int socketInit() {
+
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
+
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   portno = 5001;
+
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
+   serv_addr.sin_port = htons(portno);
+
+   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR on binding");
+      exit(1);
+   }
+
+   listen(sockfd,5);
+   clilen = sizeof(cli_addr);
+
+    return 0;
+}
+
+int handleConnections() {
+    while(1) {
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            exit(1);
+        }
+        if(!fork()) {
+            close(sockfd);
+        } else {
+            close(newsockfd);
+        }
+
+
+
+    }
+}
