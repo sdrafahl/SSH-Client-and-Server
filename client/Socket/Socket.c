aa8e9146660aa674951f1ee3d0de244dd33f4b99
Shane Drafahl <sdrafahl@sources-mbp.student.iastate.edu>
2018-09-18 20:06:42 -0500

    Added some basic socket code

---
+++ client/Socket/Socket.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8080
+
+struct sockaddr_in address;
+int sock = 0, valread;
+struct sockaddr_in serv_addr;
+char buffer[1024] = {0};
+
+int sendMessage(char* message) {
+  send(sock, message, strlen(message), 0);
+  return 0;
+}
+
+int setup() {
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+      printf("\n Socket creation error \n");
+      return -1;
+  }
+  memset(&serv_addr, '0', sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+  if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+  {
+      printf("\nInvalid address/ Address not supported \n");
+      return -1;
+  }
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+  {
+      printf("\nConnection Failed \n");
+      return -1;
+  }
+  printf("Connected \n");
+  return 0;
+}
